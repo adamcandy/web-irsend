60083050b4d8e81d4a258bab6abfd9abf56190dc
Adam Candy <adam.candy@imperial.ac.uk>
2013-03-31 16:33:24 +0100

    Added a button to change volume in increments of 10.

--- remote.py
+++ remote.py
@@ -6,6 +6,7 @@ from flask import render_template
 from flask import request, redirect, url_for
 
 BASE_URL = ''
+volumeten = False
 
 app = Flask(__name__)
 
@@ -27,21 +28,40 @@ def index(device=None):
     
     return render_template('remote.html', devices=devices)
 
-
+def volumetencolour(option):
+    volumetencolour_off = '#F18518'
+    volumetencolour_on = '#FF0000'
+    if option:
+	return volumetencolour_on
+    else:
+        return volumetencolour_off
+  
 @app.route("/device/<device_id>")
 def device(device_id=None):
     d = {'id':device_id}        
     if device_id.startswith('apple'):
-        return render_template('control_apple.html', d=d)
+        return render_template('control_apple.html', d=d, volumetencolour=volumetencolour(volumeten))
     else:
-        return render_template('control.html', d=d)
+        return render_template('control.html', d=d, volumetencolour=volumetencolour(volumeten))
 
 
 @app.route("/device/<device_id>/clicked/<op>")
 def clicked(device_id=None, op=None):
     # Send message to Lirc to control the IR
-    lircParse.send_once(device_id, op)
-    
+    if (op.startswith('volume') and volumeten):
+        op = op + ( ' ' + op ) * 9
+        lircParse.send_once(device_id, op)
+    else:	
+        lircParse.send_once(device_id, op)
+    return ""
+
+@app.route("/device/<device_id>/clicked/volumeten/<op>")
+def clickedvolumetenoption(device_id=None, op='off'):
+    global volumeten
+    if op == 'on':
+        volumeten = True
+    else:
+        volumeten = False
     return ""
 
 
--- static/remote.js
+++ static/remote.js
@@ -11,4 +11,23 @@ function clickedOp(device_id, op)
     $.ajax({url:device_id + '/clicked/' + op});
 }
 
+function clickedvolumeten(device_id)
+{
+    volumetencolour_off = '0.0';
+    volumetencolour_on = '1.0';
+    option = document.getElementById('volumeten').getAttribute('opacity');
+    if (option == volumetencolour_on) {
+        document.getElementById('volumeten').setAttribute('opacity', volumetencolour_off);
+    	$.ajax({url:device_id + '/clicked/volumeten/off'});
+    } else {
+        document.getElementById('volumeten').setAttribute('opacity', volumetencolour_on);
+    	$.ajax({url:device_id + '/clicked/volumeten/on'});
+    }
+
+ 
+    //document.getElementById('volumeten').setAttribute('fill', 'yellow');
+    //$('volumeten').style.fill="yellow";
+
+}
+
 
